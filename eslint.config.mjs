f66a5968e878a43a6de4ff49e679b1c7bc6ef02f
Mark Sayson <masayson@gmail.com>
2025-01-13 00:13:35 -0800

    Update eslint config
    Enable additional lint rules and update lint command to
    automatically fix lint errors when possible.
    
    Fix lint errors, with one exception added for the default
    main.tsx file to avoid correctness issues, can investigate
    resolving the no-non-null-assertion violation later.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,53 @@
-// @ts-check
-
-import eslint from '@eslint/js';
+import js from '@eslint/js';
+import globals from 'globals';
 import react from 'eslint-plugin-react';
+import reactHooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 
-// Ref: https://typescript-eslint.io/getting-started/typed-linting/
 export default tseslint.config(
-  eslint.configs.recommended,
-  tseslint.configs.strictTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
   {
+    ignores: [
+      'dist',
+      'coverage',
+      'vite.config.ts'
+    ]
+  },
+  {
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.strictTypeChecked,
+      ...tseslint.configs.stylisticTypeChecked
+    ],
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
       parserOptions: {
-        projectService: true,
+        project: ['./tsconfig.json'],
         tsconfigRootDir: import.meta.dirname,
       }
     },
     plugins: {
-      react,
+      'react-hooks': reactHooks,
     },
     rules: {
-      ...react.configs.recommended.rules,
-      ...react.configs['jsx-runtime'].rules,
+      ...reactHooks.configs.recommended.rules,
+      '@typescript-eslint/no-confusing-void-expression': 'off',
+      'semi': ['error', 'always'],
+      'quotes': ['error', 'single'],
+      'indent': ['error', 2],
     },
-  }
+    settings: {
+      react: {
+        version: 'detect',
+      },
+      plugins: {
+        react,
+      },
+      rules: {
+        ...react.configs.recommended.rules,
+        ...react.configs['jsx-runtime'].rules,
+      }
+    }
+  },
 );
