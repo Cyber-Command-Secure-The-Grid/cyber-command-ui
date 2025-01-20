08e355a88961230d92a683712eff47482a735923
Mark Sayson <masayson@gmail.com>
2025-01-19 23:59:34 -0800

    Update styling for better mobile experience

--- src/components/DialogNextButton.tsx
+++ src/components/DialogNextButton.tsx
@@ -1,7 +1,7 @@
 import { DialogOnOptionSelect, DialogOption } from '../types/Dialog';
 
 export function DialogNextButton(index: number, onOptionSelect: DialogOnOptionSelect, option: DialogOption) {
-  return <button className="dialogue-next-button" key={index}
+  return <button className="game-button" key={index}
     onClick={() => onOptionSelect({
       nextDialogId: option.nextDialogId,
       nextSecurityConsoleId: option.nextSecurityConsoleId
