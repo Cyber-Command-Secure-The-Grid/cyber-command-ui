48b3baee5e58084865629eacd038e261f159aed5
Mark Sayson <masayson@gmail.com>
2025-01-19 16:19:44 -0800

    Update base URL to have trailing forward slash
    GitHub Pages may require base URLs to have a trailing
    forward slash in order to correctly interpret asset
    paths, while this isn't necessary to run the web
    app locally.
    
    Updating to enable deployments to GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/cyber-command-ui',
+  base: '/cyber-command-ui/',
   plugins: [react()],
 });
